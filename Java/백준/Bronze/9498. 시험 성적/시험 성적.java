ba19648d71b7d617d31dc1f48fec1dda28780d95
20210699 <102691173+20210699@users.noreply.github.com>
2025-01-06 23:41:00 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,34 @@
+//시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        
+        String grade;
+        
+        switch(N / 10) {
+            case 10:
+            case 9:
+                grade = "A";
+                break;
+            case 8:
+                grade = "B";
+                break;
+            case 7:
+                grade = "C";
+                break;
+            case 6:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+                break;
+        }
+        System.out.println(grade);
+    }
+}
