56b0a93f1a8c5d595839ab08ea9056bfa4e904eb
20210699 <102691173+20210699@users.noreply.github.com>
2025-01-18 22:56:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 92 ms, Memory: 14308 KB -BaekjoonHub

---
+++ Java/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+// 2541 -> 1998
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer year = Integer.parseInt(br.readLine());
+        System.out.println(year - 543);
+    }
+}
