1cc50f18c4eb9bfbcb883c9c7b92b399d88d3ebd
20210699 <102691173+20210699@users.noreply.github.com>
2025-01-19 23:30:13 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
